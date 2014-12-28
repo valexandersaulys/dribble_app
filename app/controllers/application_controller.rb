e17bc4d17f09411801548d11bef64fe81724d87a
Vincent A. Saulys <valexandersaulys@gmail.com>
2014-12-27 19:30:56 -0500

    Added Name to Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  # From Devise =======================
+  # Adds parameters to Account Sign-Up and Update
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) << :name
+  	devise_parameter_sanitizer.for(:account_update) << :name
+  end
+
 end
