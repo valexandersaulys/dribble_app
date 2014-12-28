a5ab6695f52791d94aa452459c24ce956e72b5b3
Vincent A. Saulys <valexandersaulys@gmail.com>
2014-12-27 20:02:21 -0500

    Added Basic Comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :posts
+  resources :posts do
+  	resources :comments		#nested routes so comments are only visible through posts
+  end
 
   root 'posts#index'
 
